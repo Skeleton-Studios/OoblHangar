8cf12f746e3c91a5d7f9a52da24ac211bc9a6e0f
a5tronomy <67793178+a5tronomy@users.noreply.github.com>
2025-06-17 00:08:44 +0200

    Update KawaiiPhysicsDynamicBoneDataAsset.cpp

--- Plugins/KawaiiPhysics/Source/KawaiiPhysics/Private/KawaiiPhysicsDynamicBoneDataAsset.cpp
+++ Plugins/KawaiiPhysics/Source/KawaiiPhysics/Private/KawaiiPhysicsDynamicBoneDataAsset.cpp
@@ -2,4 +2,11 @@
 
 UKawaiiPhysicsDynamicBoneDataAsset::UKawaiiPhysicsDynamicBoneDataAsset()
 {
+    DummyBoneLength = 0.00f;
+    BoneForwardAxis = EBoneForwardAxis::X_Positive;
+    PlanarConstraint = EPlanarConstraint::None;
+    TeleportDistanceThreshold = 300.00f;
+    TeleportRotationThreshold = 10.00f;
+    bEnableWind = false;
+    WindScale = 1.00f;
 };
